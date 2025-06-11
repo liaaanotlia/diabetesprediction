4afba82ab035eada55937327f714751c4d9f9bc9
liaaanotlia <139047109+liaaanotlia@users.noreply.github.com>
2025-06-11 16:06:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
- import streamlit as st
- import numpy as np
- import joblib
- model = joblib.load('diabetes_model.pkl')
- st.title("Prediksi Diabetes")
- # Form input
- with st.form("form_diabetes"):
+import streamlit as st
+import numpy as np
+import joblib
+
+# Load model
+model = joblib.load('diabetes_model.pkl')
+
+st.title("Prediksi Diabetes")
+
+# Form input
+with st.form("form_diabetes"):
     pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1)
     glucose = st.number_input('Glucose', min_value=0, max_value=200)
     blood_pressure = st.number_input('Blood Pressure', min_value=0, max_value=150)
@@ -14,14 +18,16 @@
     dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.5)
     age = st.number_input('Age', min_value=1, max_value=120)
     submit = st.form_submit_button("Proses")
+
 # Ketika tombol ditekan
- if submit:
+if submit:
     # Format input ke bentuk array
-    features = np.array([[pregnancies, glucose, 
-blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
-    
+    features = np.array([[pregnancies, glucose, blood_pressure,
+                          skin_thickness, insulin, bmi, dpf, age]])
+
     # Prediksi
     prediction = model.predict(features)[0]
+
     # Tampilkan hasil
     if prediction == 1:
         st.error("Hasil: Positif Diabetes")
